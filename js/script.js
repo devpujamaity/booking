5027af6b5cffeff6154f4e898a3429c50d78290e
devpujamaity <166712470+devpujamaity@users.noreply.github.com>
2024-06-13 14:27:03 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$('.slide').slick({
+    dots: false,
+    arrow: true,
+    fade: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+});
+
+$('.js-exciting-exp-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 3000,
+});
+
+$('.happy-slide').slick({
+    dots: false,
+    arrows: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
